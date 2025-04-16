4ab6f1d3831b7c6f8ab397e56897443d36bee71c
Pendela <pranay.bhargav@capgemini.com>
2025-04-16 22:37:04 +0530

    path change

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,3 +1,3 @@
  //export const API_URL = "http://localhost:4000"
 
-export const API_URL = "https://backend-nodejs-suby-uv64.onrender.com/"
+export const API_URL = "https://backend-nodejs-suby-uv64.onrender.com"
